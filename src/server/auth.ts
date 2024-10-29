cc34315771cc199d485e9bd77d10057f236f1bad
uyuuuuu <soufflesumire.03@gmail.com>
2024-10-29 16:40:24 +0900

    fix: !を追加

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,8 +48,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID! || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! || "",
     }),
     /**
      * ...add more providers here.
